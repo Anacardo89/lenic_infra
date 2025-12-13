f007392e8e9a5df1f3b63096b3390b9be5b43be3
NAME <EMAIL>
2025-12-13 10:43:50 +0000

    removes all vars from backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "${var.project_name}-${var.environment}-terraform-state"
+    bucket         = "lenic-dev-terraform-state"
     key            = "dev/terraform.tfstate"
     region         = "eu-west-3"
     dynamodb_table = "lenic-env-terraform-lock"
