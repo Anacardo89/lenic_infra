e7f4acc2595adb3536bd5704ecda9bdc4dcbfe3e
NAME <EMAIL>
2025-12-13 10:41:18 +0000

    removes var from aws region in backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "${var.project_name}-${var.environment}-terraform-state"
     key            = "dev/terraform.tfstate"
-    region         = "${var.aws_region}"
+    region         = "eu-west-3"
     dynamodb_table = "lenic-env-terraform-lock"
     encrypt        = true
   }
